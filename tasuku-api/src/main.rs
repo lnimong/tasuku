74be6465248565ce05dbfef3d322da696df4adbf
Lionel Nimong <lionelnimong@lionels-mbp.home>
2024-06-28 18:38:37 +0200

    port -> 80

--- tasuku-api/src/main.rs
+++ tasuku-api/src/main.rs
@@ -12,7 +12,7 @@ async fn main() {
         .route("/", get(root));
 
     // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:80").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
