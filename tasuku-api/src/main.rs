bc909e96d34acaf6e03620e07276201eb40b678d
Lionel Nimong <lionelnimong@lionels-mbp.home>
2024-06-28 19:05:55 +0200

    use env

--- tasuku-api/src/main.rs
+++ tasuku-api/src/main.rs
@@ -1,3 +1,4 @@
+use std::env;
 use axum::{
     routing::{get, post},
     http::StatusCode,
@@ -6,13 +7,21 @@ use axum::{
 
 #[tokio::main]
 async fn main() {
+
+    let localhost = "0.0.0.0".to_owned();
+
+    let port = match env::var_os("PORT") {
+        Some(v) => v.into_string().unwrap(),
+        None => "80".to_string()
+    };
+
     // build our application with a route
     let app = Router::new()
         // `GET /` goes to `root`
         .route("/", get(root));
 
     // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:80").await.unwrap();
+    let listener = tokio::net::TcpListener::bind(localhost+":"+&port).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
